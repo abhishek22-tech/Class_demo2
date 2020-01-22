9baaf979f37f1aa78797324a63187cf5f9803b30
Abhishek Singh <abhishek22@usf.edu>
2020-01-21 20:30:05 -0500

    Comment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Class_demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets start this!!");
+            Console.ReadLine();
+        }
+    }
+}
